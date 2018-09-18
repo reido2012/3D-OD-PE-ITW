2ba11ba9bdaefc5476040d326fd2b4c15da3c995
root <root@instance-1-gpu-omar.c.deep-learning-experiments.internal>
2018-09-18 10:35:48 +0000

    Add a few things

--- create_tfrecords.py
+++ create_tfrecords.py
@@ -3,4 +3,4 @@ import pascal3d
 data_type = 'all'
 dataset = pascal3d.dataset.Pascal3DDataset(data_type, generate=False)
 #print("Dataset Length: {}".format(len(dataset)))
-dataset._create_tfrecords_from_data_ids('airplanes.tfrecords', ['2008_003140', '2010_001174'])
+dataset.create_tfrecords()
