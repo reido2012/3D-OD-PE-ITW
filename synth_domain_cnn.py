ef8994dbdf21ae49446d9114aa226d3865cdd372
Omar Reid <oreid52@googlemail.com>
2019-03-23 13:13:38 +0000

    attempt to fix weight loading

--- synth_domain_cnn.py
+++ synth_domain_cnn.py
@@ -25,13 +25,6 @@ RESNET_V1_CHECKPOINT_DIR = "/home/omarreid/selerio/datasets/pre_trained_weights/
 def synth_domain_cnn_model_fn(features, labels, mode):
     rgb_images, positive_depth_images, negative_depth_images = features
 
-    with tf.variable_scope('synth_domain', reuse=tf.AUTO_REUSE):
-        with slim.arg_scope(resnet_v1.resnet_arg_scope()):
-            # Retrieve the function that returns logits and endpoints - ResNet was pre trained on ImageNet
-            network_fn = nets_factory.get_network_fn(NETWORK_NAME, num_classes=None, is_training=True)
-
-            positive_depth_descriptors, endpoints = network_fn(positive_depth_images)
-            negative_depth_descriptors, endpoints = network_fn(negative_depth_images)
 
     rgb_descriptors = get_pretrained_resnet_descriptors(rgb_images, is_training=True)
 
@@ -41,6 +34,14 @@ def synth_domain_cnn_model_fn(features, labels, mode):
     print(real_domain_variables_to_restore)
     checkpoint_path = tf.train.latest_checkpoint(PRETRAINED_MODEL_DIR)
     tf.train.init_from_checkpoint(checkpoint_path, {v.name.split(':')[0]: v for v in real_domain_variables_to_restore})
+
+    # with tf.variable_scope('synth_domain', reuse=tf.AUTO_REUSE):
+    with slim.arg_scope(resnet_v1.resnet_arg_scope()):
+        # Retrieve the function that returns logits and endpoints - ResNet was pre trained on ImageNet
+        network_fn = nets_factory.get_network_fn(NETWORK_NAME, num_classes=None, is_training=True, reuse=True)
+
+        positive_depth_descriptors, endpoints = network_fn(positive_depth_images)
+        negative_depth_descriptors, endpoints = network_fn(negative_depth_images)
 
     variables_to_restore = slim.get_variables_to_restore(include=['synth_domain'], exclude=['real_domain'])
     print("New Synth Variables")
@@ -52,7 +53,7 @@ def synth_domain_cnn_model_fn(features, labels, mode):
         checkpoint_path = RESNET_V1_CHECKPOINT_DIR
         variables_to_restore = [v for v in variables_to_restore if 'resnet_v1_50/' in v.name and 'real_domain/' not in v.name and 'synth_domain/' in v.name]
 
-    tf.train.init_from_checkpoint(checkpoint_path, {'synth_domain/' + v.name.split(':')[0]: v for v in variables_to_restore})
+    tf.train.init_from_checkpoint(checkpoint_path, {v.name.split(':')[0]: v for v in variables_to_restore})
 
     loss = similarity_loss(rgb_descriptors, positive_depth_descriptors, negative_depth_descriptors)
 
