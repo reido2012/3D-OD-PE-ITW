3ed81093228c15ebf09a653cbbd744e0cc438813
Omar Reid <oreid52@googlemail.com>
2019-04-02 10:53:51 +0100

    Re creating tfrecords

--- create_synth_tfrecords.py
+++ create_synth_tfrecords.py
@@ -2,5 +2,5 @@ import pascal3d
 
 data_type = 'all'
 dataset = pascal3d.dataset.Pascal3DDataset(data_type, dataset_path="/home/omarreid/selerio/datasets/PASCAL3D+_release1.1")
-dataset.create_tfrecords_synth_domain("/home/omarreid/selerio/datasets/synth_domain_tfrecords_new/", True)
+dataset.create_tfrecords_synth_domain("/home/omarreid/selerio/datasets/synth_domain_tfrecords/", True)
 
