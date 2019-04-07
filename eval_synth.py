70e4b4d6f267f12beabc25f9fda0c1f3151b078b
Omar Reid <oreid52@googlemail.com>
2019-04-07 12:51:17 +0100

    Debug

--- eval_synth.py
+++ eval_synth.py
@@ -63,14 +63,14 @@ def visualize_embeddings(tfrecords_file):
         print(pos_embeddings.shape)
         # Visualize test embeddings
         # pos_embedding_var = tf.identity(pos_embeddings, name="pos_depth")
-        pos_embedding_var = tf.Variable(pos_embeddings, name='pos_depth')
+        # pos_embedding_var = tf.Variable(pos_embeddings, name='pos_depth')
 
         eval_dir = os.path.join(MODEL_DIR, "eval")
         summary_writer = tf.summary.FileWriter(eval_dir)
 
         config = projector.ProjectorConfig()
         embedding = config.embeddings.add()
-        embedding.tensor_name = pos_embedding_var.name
+        embedding.tensor_name = 'pos_depth'
 
         embedding.sprite.image_path = 'pos_depth_sprite.png'
         embedding.sprite.single_image_dim.extend([224, 224])
@@ -80,7 +80,7 @@ def visualize_embeddings(tfrecords_file):
 
         with tf.Session() as sess:
             saver = tf.train.Saver()
-            sess.run(pos_embedding_var.initializer)
+            # sess.run(pos_embedding_var.initializer)
             saver.save(sess, os.path.join(eval_dir, "pos_embeddings.ckpt"))
 
 
