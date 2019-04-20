edd6c3d1b73c3230b6525df4d78e7b0e4bab4b31
Omar Reid <oreid52@googlemail.com>
2019-04-20 10:34:25 +0100

    Fix Writing

--- create_tfrecords.py
+++ create_tfrecords.py
@@ -3,4 +3,4 @@ import pascal3d
 data_type = 'all'
 dataset = pascal3d.dataset.Pascal3DDataset(data_type, dataset_path="/home/omarreid/selerio/datasets/PASCAL3D+_release1.1", generate=True)
 #print("Dataset Length: {}".format(len(dataset)))
-dataset.create_tfrecords("/home/omarreid/selerio/final_year_project/real_domain_tfrecords/")
+dataset.create_tfrecords("/home/omarreid/selerio/datasets/real_domain_tfrecords/")
